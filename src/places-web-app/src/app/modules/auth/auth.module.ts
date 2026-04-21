22761fa6306203fbb9f0f53885096c103d9a07a6
Fernando Ricardo Moran <ocierd@msn.com>
2026-04-20 21:13:47 -0600

    SharedModule & Formulario Login

--- src/places-web-app/src/app/modules/auth/auth.module.ts
+++ src/places-web-app/src/app/modules/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent, RegistroComponent],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, SharedModule],
 })
 export class AuthModule {}
