215c12bef6fd507db71b54f5d3e1e13e8747ba64
Fernando Ricardo <ocierd@msn.com>
2026-04-23 20:09:02 -0600

    Directiva lower y upper para NgControl

--- src/places-web-app/src/app/shared/directives/upper/upper.directive.ts
+++ src/places-web-app/src/app/shared/directives/upper/upper.directive.ts
@@ -1,4 +1,5 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, Optional } from '@angular/core';
+import { AbstractControl, NgControl } from '@angular/forms';
 
 /**
  * Directiva utilizada para convertir a mayúsculas el texto ingresado.
@@ -19,18 +20,28 @@ export class UpperDirective {
   private elemento: HTMLInputElement | HTMLTextAreaElement;
 
   /**
+   * Control en el que se usa la directiva
+   */
+  private control: AbstractControl | null = null;
+
+  /**
    * Constructor de la directvica Upper
    * @param elementRef Wrapper del elemento nativo
    */
-  constructor(private elementRef: ElementRef) {
+  constructor(private elementRef: ElementRef, @Optional() private formControl: NgControl) {
     const esElementoPermitido = this.elementRef.nativeElement instanceof HTMLInputElement
       || this.elementRef.nativeElement instanceof HTMLTextAreaElement;
 
     if (!esElementoPermitido) {
-      throw new TypeError('El elemento debe ser un Input o TextArea')
+      throw new TypeError('El elemento debe ser un Input o TextArea');
     }
 
     this.elemento = this.elementRef.nativeElement;
+
+    if (this.formControl && this.formControl.control) {
+      this.control = this.formControl.control;
+    }
+
   }
 
   /**
@@ -39,15 +50,37 @@ export class UpperDirective {
    */
   cambiarAMayusculas(evento: Event) {
     console.log("Tipo evento: ", evento.type);
-
     const valorElemento = this.elemento.value;
+
     if (typeof valorElemento === 'string') {
       console.info('Valor original: ', valorElemento);
       const valorMayusculas = valorElemento.toUpperCase();
-      this.elemento.value = valorMayusculas;
       console.info("Valor transformado: ", valorMayusculas);
+      this.cambiaraMayusculasElementoNativo(valorMayusculas);
+      this.cambiarMayusculasControl(valorMayusculas);
     }
 
   }
+
+  /**
+   * Cambia el valor del elemento nativo a mayúsculas
+   * @param valorMayusculas Valor en mayúsculas
+   */
+  cambiaraMayusculasElementoNativo(valorMayusculas: string) {
+    this.elemento.value = valorMayusculas;
+  }
+
+  /**
+   * Cambia el valor del control a mayúsculas, siempre y cuando éste exista
+   * @param valorMayusculas Valor en mayúsculas
+   */
+  cambiarMayusculasControl(valorMayusculas: string) {
+    if (this.control) {
+      this.control.setValue(valorMayusculas, { emitEvent: false });
+    }
+  }
+
+
+
 
 }
