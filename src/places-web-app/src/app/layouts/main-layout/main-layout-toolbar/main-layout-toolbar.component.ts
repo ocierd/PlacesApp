505f73378c70f55b11a0263e27ae544b41c3b526
Fernando Ricardo <ocierd@msn.com>
2026-04-26 18:29:38 -0600

    Main layout distribution

---
+++ src/places-web-app/src/app/layouts/main-layout/main-layout-toolbar/main-layout-toolbar.component.ts
@@ -0,0 +1,31 @@
+import { Component, model, effect, input, InputSignal, output, ModelSignal } from '@angular/core';
+import { MaterialModule } from '@shared/material/material-module';
+
+
+/*
+  * Componente de la barra de herramientas del layout principal, que incluye un botón para abrir/cerrar el menú lateral.
+  * Este componente recibe el estado de apertura del menú lateral a través de una señal de entrada y emite eventos de cambio de estado a través de una señal de salida.
+  * El botón de menú alterna el estado de apertura del menú lateral al hacer clic en él.
+  * El componente utiliza Angular Material para la barra de herramientas y el botón.
+*/
+@Component({
+  selector: 'app-main-layout-toolbar',
+  templateUrl: './main-layout-toolbar.component.html',
+  styleUrl: './main-layout-toolbar.component.scss',
+  imports: [MaterialModule],
+  standalone: true,
+})
+export class MainLayoutToolbarComponent {
+
+  /**
+   * Señal de entrada que indica si el menú lateral está abierto o cerrado
+   */
+  opened: ModelSignal<boolean> = model.required<boolean>();
+
+  /**
+   * Señal de salida que emite el nuevo estado de apertura del menú lateral cuando se hace clic en el botón de menú
+   */
+  onClickMenu() {
+    this.opened.set(!this.opened());
+  }
+}
--- src/places-web-app/src/app/layouts/main-layout/main-layout.component.ts
+++ src/places-web-app/src/app/layouts/main-layout/main-layout.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, model, ModelSignal, signal, WritableSignal } from '@angular/core';
 import { AuthRoutingModule } from "../../modules/auth/auth-routing.module";
+import { MaterialModule } from '@shared/material/material-module';
+import { MainLayoutToolbarComponent } from "./main-layout-toolbar/main-layout-toolbar.component";
 
+
+
+/**
+ * Componente principal del layout de la aplicación, que incluye la barra de herramientas y el menú lateral.
+ * Este componente se encarga de gestionar el estado del menú lateral (abierto/cerrado) y el modo de visualización del mismo.
+ */
 @Component({
   selector: 'app-main-layout',
-  imports: [AuthRoutingModule],
+  imports: [AuthRoutingModule, MaterialModule, MainLayoutToolbarComponent],
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {}
+export class MainLayoutComponent {
+  /**
+   * Indica si el menú lateral está abierto o cerrado
+   */
+  hasBackdrop: WritableSignal<boolean> = signal(true);
+
+  /**
+   * Modo de visualización del menú lateral (over, push, side)
+   */
+  readonly mode: WritableSignal<'over' | 'push' | 'side'> = signal('over');
+
+  /**
+   * Estado de apertura del menú lateral
+   */
+  readonly opened: ModelSignal<boolean> = model(false);
+
+}
