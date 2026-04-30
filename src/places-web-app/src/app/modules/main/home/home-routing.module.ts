a87ddf5d9176c44864e8b105e351982df7642557
Fernando Ricardo <ocierd@msn.com>
2026-04-29 19:27:06 -0600

    Módulo 'Developer' y componentes de botones y autocomplete

--- src/places-web-app/src/app/modules/main/home/home-routing.module.ts
+++ src/places-web-app/src/app/modules/main/home/home-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainHomeComponent } from './pages/main-home/main-home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ButtonsComponent } from './pages/buttons/buttons.component';
-import { AutocompleteExampleComponent } from './pages/autocomplete-example/autocomplete-example.component';
 
 const routes: Routes = [
   {
@@ -19,14 +17,6 @@ const routes: Routes = [
     path: 'dashboard',
     component: DashboardComponent
   },
-  {
-    path: 'buttons',
-    component: ButtonsComponent
-  },
-  {
-    path: 'autocomplete',
-    component: AutocompleteExampleComponent
-  }
 ];
 
 @NgModule({
