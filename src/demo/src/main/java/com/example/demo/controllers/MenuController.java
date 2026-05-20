12724086bd628b1686a0007cb7c582d0702e9018
Fernando Ricardo <ocierd@msn.com>
2026-05-20 14:48:11 -0600

    Módulos y menús - Back

---
+++ src/demo/src/main/java/com/example/demo/controllers/MenuController.java
@@ -0,0 +1,61 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.domain.Menu;
+import com.example.demo.domain.Usuario;
+import com.example.demo.domain.dto.MenuDto;
+import com.example.demo.services.interfaces.MenuService;
+
+/**
+ * Controlador para manejar las operaciones relacionadas con el menú, como
+ * obtener la lista de menús disponibles y los menús específicos para un
+ * usuario.
+ * Provee endpoints para acceder a la información del menú, incluyendo la
+ * estructura jerárquica de menús y submenús.
+ * Permite a los clientes obtener la lista de menús disponibles en el sistema,
+ * así como los menús específicos para un usuario autenticado.
+ */
+@RestController
+@RequestMapping("/menu")
+public class MenuController extends BaseController {
+
+    /**
+     * Servicio para manejar las operaciones relacionadas con el menú.
+     */
+    private final MenuService menuService;
+
+    public MenuController(MenuService menuService) {
+        this.menuService = menuService;
+    }
+
+    /***
+     * Obtiene la lista de menús disponibles en el sistema.
+     * 
+     * @return La lista de menús disponibles en el sistema.
+     */
+    @GetMapping
+    public List<Menu> getMenus() {
+        return menuService.getMenus();
+    }
+
+    /**
+     * Obtiene la lista de menús disponibles para el usuario autenticado y un módulo
+     * específico (si se proporciona), organizados en una estructura jerárquica
+     * (menú padre e hijos).
+     * 
+     * @param moduloId El ID del módulo para filtrar los menús (opcional).
+     * @return La lista de menús disponibles para el usuario autenticado.
+     */
+    @GetMapping("/usuario")
+    public List<MenuDto> obtenerMenuUsuarioModulo(@RequestParam(required = false) Short moduloId) {
+        Usuario usuario = getCurrentUser();
+        return menuService.obtenerMenuUsuarioModulo(usuario.getUsuarioId(), moduloId);
+    }
+
+}
---
+++ src/demo/src/main/java/com/example/demo/domain/dto/MenuDto.java
@@ -0,0 +1,55 @@
+package com.example.demo.domain.dto;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * DTO para representar la información del menú, incluyendo sus submenús (children) y su menú padre (padre).
+ */
+@Getter
+@Setter
+public class MenuDto {
+    
+    /**
+     * ID del menú. Es un campo opcional, ya que al crear un nuevo menú no se proporcionará un ID. Sin embargo, al obtener o actualizar un menú existente, este campo será necesario para identificarlo de manera única.
+     */
+    private Integer menuId;
+
+    /**
+     * Nombre del menú. Es un campo obligatorio que representa el nombre del menú. Debe tener un valor no nulo y no vacío, con una longitud máxima de 50 caracteres.
+     */
+    private String nombre;
+
+    /**
+     * Ruta del menú. Es un campo opcional que representa la ruta o URL asociada al menú. Puede ser nulo o vacío, pero si se proporciona, debe tener una longitud máxima de 255 caracteres.
+     */
+    private String ruta;
+
+    /**
+     * Icono del menú. Es un campo opcional que representa el icono asociado al menú. Puede ser nulo o vacío, pero si se proporciona, debe tener una longitud máxima de 50 caracteres.
+     */
+    private String icono;
+
+    /**
+     * ID del menú padre. Es un campo opcional que representa el ID del menú padre al que pertenece este menú. Puede ser nulo si el menú no tiene un padre (es decir, es un menú raíz). Si se proporciona, debe ser un número entero positivo que corresponda al ID de un menú existente en la base de datos.
+     */
+    private Integer padreMenuId;
+
+    /**
+     * Menú padre. Es un campo opcional que representa el menú padre al que pertenece este menú. Puede ser nulo si el menú no tiene un padre (es decir, es un menú raíz). Si se proporciona, debe ser un objeto MenuDto que corresponda a un menú existente en la base de datos.
+     */
+    private MenuDto padre;
+
+    /**
+     * Submenús. Es un campo opcional que representa los submenús asociados a este menú. Puede ser nulo o vacío, pero si se proporciona, debe ser una lista de objetos MenuDto que correspondan a menús existentes en la base de datos.
+     */
+    private List<MenuDto> hijos;
+
+
+    /**
+     * ID del módulo al que pertenece el menú. Es un campo opcional que representa el ID del módulo al que pertenece este menú. Puede ser nulo si el menú no está asociado a ningún módulo. Si se proporciona, debe ser un número entero positivo que corresponda al ID de un módulo existente en la base de datos.
+     */
+    private Short moduloId;
+}
