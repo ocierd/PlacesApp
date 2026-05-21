4741d388dce9bb6efc8baecac7c084cc316333b8
ocierd <38734951+ocierd@users.noreply.github.com>
2026-05-20 20:28:16 -0600

    Develop/karen/registro usuario (#16)
    * Registro de usuario
    
    * Implementaciónde 'SpinnerButton'
    
    * clases Bootstrap en 'RegistroComponent'
    
    * Registro de usuario en distintos componentes
    
    * Filtrado de países desde back
    
    * Generación del componente para el registro de usuario
    
    ---------
    
    Co-authored-by: Karen <anakarengg0790@gmail.com>

--- src/places-web-app/src/app/modules/auth/pages/registro/registro.component.ts
+++ src/places-web-app/src/app/modules/auth/pages/registro/registro.component.ts
@@ -3,6 +3,12 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { DatosPersonalesRegistroStepComponent } from '@modules/auth/components/datos-personales-registro-step/datos-personales-registro-step.component';
 import { DatosContactoRegistroStepComponent } from '@modules/auth/components/datos-contacto-registro-step/datos-contacto-registro-step.component';
 import { CredencialesRegistroStepComponent } from '@modules/auth/components/credenciales-registro-step/credenciales-registro-step.component';
+import { Usuario, UsuarioRegistroDto } from '@shared/models/usuario.model';
+import { LoggerService } from '@shared/services/logger/logger.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { UsuarioService } from '@services/usuario/usuario.service';
+import { firstValueFrom } from 'rxjs';
+import { Pais } from '@shared/models/pais.model';
 
 
 /**
@@ -14,9 +20,10 @@ import { CredencialesRegistroStepComponent } from '@modules/auth/components/cred
   templateUrl: './registro.component.html',
   styleUrl: './registro.component.scss',
 
+
 })
 export class RegistroComponent {
-  
+
 
   @ViewChild('datosPersonalesStep')
   datosPersonalesStep!: DatosPersonalesRegistroStepComponent
@@ -31,7 +38,9 @@ export class RegistroComponent {
 
   constructor(
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private logger: LoggerService,
+    private usuarioService: UsuarioService
   ) { }
 
   /**
@@ -49,6 +58,50 @@ export class RegistroComponent {
     }
 
     this.router.navigate(['../login'], { relativeTo: this.route });
+  }
+
+
+
+  async crearUsuario(): Promise<void> {
+    try {
+      const datosContacto:{
+        telefono:string,
+        email:string,
+        pais:Pais
+      }=this.datosContactoStep.datosContactoForm.getRawValue();
+
+      const nuevoRegistro = {
+        ...this.credencialesStep.credencialesFormGroup.getRawValue(),
+        ...datosContacto,
+        ...this.datosPersonalesStep.datosPersonalesForm.getRawValue(),
+        paisId:datosContacto.pais.paisId
+      } as UsuarioRegistroDto
+
+      console.log(nuevoRegistro)
+
+      const usuario = await firstValueFrom(
+        this.usuarioService.crearUsuario(nuevoRegistro)
+      );
+
+      console.log("Usuario creado:", usuario);
+
+    } catch (error) {
+
+      this.logger.error("Error en registro:", error);
+
+      if (error instanceof HttpErrorResponse) {
+
+        alert("Error: " + error.message);
+
+      } else {
+
+        this.logger.error("Error desconocido:", error);
+
+      }
+
+    } finally {
+
+    }
   }
 }
 
